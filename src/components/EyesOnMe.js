cde5d518340f03e0b95c4becf0ce43848a2a85b5
darlene9497 <darlene.nandabi@student.moringaschool.com>
2023-01-11 15:06:39 +0300

    Complete Assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe(){
+    return (
+    <button onFocus={handleFocus} 
+            onBlur={handleBlur}>Eyes on me
+    </button>
+)
+    function handleFocus(){
+        console.log('Good!')
+    }
+    function handleBlur(){
+        console.log('Hey! Eyes on me!')
+    }
+
+}
+
+export default EyesOnMe;
